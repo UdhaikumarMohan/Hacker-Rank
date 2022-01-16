ae22d4e7207b337a3081dd69e96e80d143015507
Udhaikumar Mohan <udhaikumar4@gmail.com>
2022-01-16 17:15:01 +0530

    Grade

--- Mark List.py
+++ Mark List.py
@@ -1,2 +1,51 @@
 
-# Print the Name of 2nd Rank holders in Alphabetical Order:
+# Print the Name of 2nd Lowest Grade in Alphabetical Order:
+
+
+if __name__ == '__main__':
+    Mark_Sheet = {}
+    Strength = int(input("Enter the total No. of students: "))
+    for N in range(Strength):
+        name = input("Name of the Student: ")
+        score = float(input("Marks Scored: "))
+        
+        Mark_Sheet[name]=score
+
+    print(Mark_Sheet)
+        
+    Marks = []
+    for x, y in Mark_Sheet.items():
+
+        Marks.append(y)
+
+    minimum = min(Marks)
+    sec = max(Marks)
+
+    for a in Marks:
+
+        if a > minimum or a <= sec:
+
+            sec = a
+
+    print(sec)
+
+    Name_List = []
+
+    for a in Mark_Sheet:
+
+        if Mark_Sheet[a] == sec:
+
+            Name_List.append(a)
+
+
+    Name_List.sort()
+
+    for a in Name_List:
+
+        print(a)
+
+
+
+
+
+    
